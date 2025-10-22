6030ea88cfd78dd55ae291c14442a336015db652
Derek Gooding <djgooding@live.com>
2025-10-22 14:26:22 -0400

    add usings with new namespaces

--- Common/DbConnectionHelper.cs
+++ Common/DbConnectionHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using MES.Common.Exceptions;
+using System.Text.Json;
 
 namespace MES.Common;
 
--- Common/ValidateConfig.cs
+++ Common/ValidateConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using MES.Common.Exceptions;
+using System.Net;
 
 namespace MES.Common;
 
